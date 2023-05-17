e3251099576db6049b0303c2c61aca91decf2299
XvorionZeta <zulfikar.h9050@gmail.com>
2023-05-17 15:41:26 +0700

    batch insert

--- app/Http/Livewire/AddIndikatorPenilaianController.php
+++ app/Http/Livewire/AddIndikatorPenilaianController.php
@@ -18,30 +18,71 @@ class AddIndikatorPenilaianController extends Component
 
     public $saved = false;
 
+    public function mount()
+    {
+        $this->end["sub0"] = [''];
+        $this->start["sub0"] = [''];
+        $this->formEvaluasi["sub0"] = [''];
+    }
+
     public function render()
     {
+        // dd($this->formEvaluasi);
         return view('livewire.add-indikator-penilaian');
     }
 
-    public function deleteEvaluasi($idSub)
+    public function deleteEvaluasi($idSub, $index)
     {
+        dd($this->end);
+        if($this->formEvaluasi[$idSub] ?? null)
+        {
+            unset($this->end[$idSub."-".$index]);
+            unset($this->start[$idSub."-".$index]);
+            unset($this->formEvaluasi[$idSub][$index]);
+
+            // $this->end = array_values($this->end);
+            // $this->start = array_values($this->start);
+            $this->formEvaluasi[$idSub] = array_values($this->formEvaluasi[$idSub]);
+        }
+        else
+        {
+            //
+        }
     }
 
-    public function addEvaluasi()
+    public function addEvaluasi($idSub, $index)
     {
-        dd($this->formEvaluasi['sub00']);
-        $this->formEvaluasi[] = '';
+        if($this->formEvaluasi[$idSub] ?? null)
+        {
+            $this->start[$idSub][] = '';
+            $this->end[$idSub][] = '';
+            $this->formEvaluasi[$idSub][] = '';
+        }
+        else
+        {
+            $this->start[$idSub] = [''];
+            $this->end[$idSub] = [''];
+            $this->formEvaluasi[$idSub] = [''];
+        }
+        // dd($this->formEvaluasi);
     }
 
     public function deleteIndikator($index)
     {
         // dd(array_values($this->subIndikators));
+        unset($this->maxPoin[$index]);
+        unset($this->end["sub".$index]);
+        unset($this->start["sub".$index]);
         unset($this->subIndikators[$index]);
+        $this->maxPoin = array_values($this->maxPoin);
         $this->subIndikators = array_values($this->subIndikators);
     }
 
-    public function addIndikator()
+    public function addIndikator($index)
     {
         $this->subIndikators[] = '';
+        $this->end["sub".$index+1] = [''];
+        $this->start["sub".$index+1] = [''];
+        $this->formEvaluasi["sub".$index+1] = [''];
     }
 }
